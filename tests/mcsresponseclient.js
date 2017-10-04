baace9a9fb2bcafb6a799aaad47877f1d7f6ee94
Rodrigo Okido <rodrigookido@gmail.com>
2017-10-04 16:14:38 -0300

    Refactoring tests

--- tests/mcsresponseclient.js
+++ tests/mcsresponseclient.js
@@ -1,17 +1,55 @@
 var MCSResponseClient = require('../lib/MCSResponseClient');
+var mcs = require('../index');
+
+var _port = 8082;
+const _host = 'localhost';
+const _timeout = 2000;
 
 exports.joinedEmptyUserId = function (test) {
-  var client = new MCSResponseClient();
-  test.throws(function () {
-    client.joined('');
-  }, Error);
-  test.done();
+  var server = new mcs.Server({port: _port});
+  var client = new mcs('ws://' + _host + ':' + _port++);
+
+  setTimeout(function () {
+    test.ok(false,'Server timeout');
+    client.closeConnection();
+    server.closeConnection();
+    test.done();
+  }, _timeout);
+
+  server.on('connection', function (rclient) {
+    rclient.on('join', () => {
+      test.throws(function () {
+        rclient.joined('');
+      }, Error);
+      test.done();
+      server.closeConnection();
+      client.closeConnection();
+    });
+  });
+
+  client.on('open', function () {
+    client.join('1','Joao',{});
+    client.on('joined', () => {
+      test.ok(true, 'Joined is working');
+      test.done();
+      server.closeConnection();
+      client.closeConnection();
+  });
+});
 }
 
 exports.joinedNullUserId = function (test) {
   var client = new MCSResponseClient();
   test.throws(function () {
     client.joined(null);
+  }, Error);
+  test.done();
+}
+
+exports.publishedAndSubscribedEmptySdp = function (test) {
+  var client = new MCSResponseClient();
+  test.throws(function () {
+    client.publishedAndSubscribed('');
   }, Error);
   test.done();
 }
